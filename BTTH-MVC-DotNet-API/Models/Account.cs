b7f3a73781618e543e1249a2010534b7c4d802ac
tungnk123 <doanthanhtungnk123@gmail.com>
2024-10-28 13:19:30 +0700

    Add dependency and models in API

---
+++ BTTH-MVC-DotNet-API/Models/Account.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BTTH_MVC_DotNet_API.Models;
+
+public partial class Account
+{
+    public string UserName { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+}
